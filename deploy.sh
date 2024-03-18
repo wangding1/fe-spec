6a3142879fc4136686fc4671dd734148c6742505
wangding <1262935861@qq.com>
2024-03-18 21:38:34 +0800

    fix: 更新

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,22 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+
+push_addr=`git remote get-url --push origin`
+commit_info=`git describe --all --always --long`
+dist_path=docs/.vuepress/dist
+push_branch=gh-pages
+
 # 生成静态文件
 npm run docs:build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+cd $dist_path
 
 git init
 git add -A
-git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:wangding1/fe-spec.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git commit -m "deploy, $commit_info"
+git push -f $push_addr HEAD:$push_branch
 
 cd -
+rm -rf $dist_path
